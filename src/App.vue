f0abf9517dc96f70b6473434b49f2075a349fb43
温鑫 <2315137135@qq.com>
2023-05-26 17:00:02 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,47 @@
-<script setup lang="ts">
-</script>
-
 <template>
+  <canvas id="bjs">
+
+  </canvas>
 
 </template>
 
+
+<script lang="ts" setup>
+import {onMounted, onUnmounted} from "vue";
+import {Engine, MeshBuilder, Scene} from "@babylonjs/core";
+import "@babylonjs/inspector"
+
+async function createScene(scene) {
+  scene.debugLayer.show().then()
+  let box = MeshBuilder.CreateBox("box")
+  scene.createDefaultCameraOrLight(true, true, true)
+}
+
+onMounted(() => {
+  let engine = new Engine(document.querySelector("#bjs") as HTMLCanvasElement)
+  let scene = new Scene(engine)
+
+  let renderLoop = function () {
+    scene.render()
+  }
+
+  createScene(scene).then(value => {
+    engine.runRenderLoop(renderLoop)
+  })
+
+  onUnmounted(() => {
+    scene.dispose()
+    engine.dispose()
+  })
+})
+
+</script>
+
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#bjs {
+  width: 100%;
+  height: 100%;
+  display: block;
 }
 </style>
